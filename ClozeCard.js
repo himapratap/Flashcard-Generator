ecb5fb5a37c7ca00f86c5f5ca6b643dffcf47d66
himapratap <himapratap@gmail.com>
2017-04-26 21:43:44 -0400

    Basic Working solution. Questions loaded from json file. User prompted with questions.

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,3 +1,5 @@
+var card;
+
 var ClozeCard = function(text, cloze) {
 
     if (this instanceof ClozeCard) {
@@ -5,11 +7,29 @@ var ClozeCard = function(text, cloze) {
         this.cloze = cloze;
         if (text.indexOf(cloze) != -1) {
             this.partial = text.replace(cloze, '...');
-
         } else {
             var err = "ERROR:'" + text + "' does not contain the word '" + cloze + "'";
             throw err;
         }
+        // console.log("Prompting..");
+        // card = this;
+        // this.prompt = function() {
+        //     inquirer.prompt([{
+        //         type: 'input',
+        //         message: this.partial,
+        //         name: "answer",
+        //     }]).then(function(input) {
+        //         console.log(card.cloze);
+        //         console.log(input.answer);
+        //         if (input.answer == card.cloze) {
+        //             console.log("yaaay");
+        //         } else {
+        //             console.log("noooo");
+        //         }
+        //     });
+        // }
+        // console.log("Prompted..");
+
     } else {
         return new ClozeCard(text, cloze);
     }
--- main.js
+++ main.js
@@ -1,30 +1,116 @@
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
+var inquirer = require("inquirer");
+var data = require("./questions.json");
 
 try {
-    var firstPresident = BasicCard(
-        "Who was the first president of the United States?", "George Washington");
+    var clozeCards = [];
+    var basicCards = [];
 
-    // "Who was the first president of the United States?"
-    console.log(firstPresident.front);
+    var currentCard;
 
-    // "George Washington"
-    console.log(firstPresident.back);
+    var cardNo = 0;
+    var score = 0;
 
-    var firstPresidentCloze = ClozeCard(
-        "George Washington was the first president of the United States.", "George Washington");
+    function playClozeCards() {
+        currentCard = clozeCards[cardNo];
+        inquirer.prompt([{
+            type: 'input',
+            message: currentCard.partial,
+            name: "answer",
+        }]).then(function(input) {
+            if (input.answer == currentCard.cloze) {
+                console.log("That's the correct answer!!");
+                score++;
+            } else {
+                console.log("Sorry, the correct answer is: ", currentCard.cloze);
+            }
+            console.log("=============================================================");
+            cardNo++;
+            if (cardNo < clozeCards.length) {
+                playClozeCards();
+            } else {
+                console.log("Hey your score is : ", score);
 
-    // "George Washington"
-    console.log(firstPresidentCloze.cloze);
+            }
+        });
 
-    // " ... was the first president of the United States.
-    console.log(firstPresidentCloze.partial);
 
-    // "George Washington was the first president of the United States."
-    console.log(firstPresidentCloze.fullText);
+    }
 
-    // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-    var brokenCloze = new ClozeCard("This doesn't work", "oops")
+    function playBasicCards() {
+        console.log(" Playing card :", cardNo);
+        currentCard = basicCards[cardNo];
+        inquirer.prompt([{
+            type: 'input',
+            message: currentCard.front,
+            name: "answer",
+        }]).then(function(input) {
+            if (input.answer == currentCard.back) {
+                console.log("That's the correct answer!!");
+                score++;
+
+            } else {
+                console.log("Sorry, the correct answer is: ", currentCard.back);
+            }
+            console.log("=============================================================");
+
+            cardNo++;
+            if (cardNo < basicCards.length) {
+                playBasicCards();
+            } else {
+                console.log("Hey your score is : ", score);
+            }
+        });
+
+
+    }
+    var cards = []
+
+    function initGame() {
+        loadCards();
+        console.log(" Hey, Let us play the World Fact !");
+        inquirer.prompt([{
+            type: 'list',
+            message: 'So what do you want to play today?',
+            name: "choice",
+            choices: ["1.Basic Card Game", "2. Cloze Card Game", "3. Na , I'm leaving"]
+        }]).then(function(input) {
+            console.log(input.choice);
+            var choice = input.choice;
+            switch (choice) {
+                case "3. Na , I'm leaving":
+                    console.log("Have a great day ahead");
+                    break;
+                case '1.Basic Card Game':
+                    playBasicCards();
+                    break;
+
+                case '2. Cloze Card Game':
+                    playClozeCards();
+                    break;
+            }
+
+        });
+
+
+    }
+
+    function loadCards() {
+        console.log('Loading Cards ..');
+        var clozeCardData = data.clozeCards;
+        for (var i = 0; i < clozeCardData.length; i++) {
+            clozeCards.push(ClozeCard(clozeCardData[i].text, clozeCardData[i].cloze));
+        }
+        var basicCardData = data.basicCards;
+        for (var i = 0; i < basicCardData.length; i++) {
+            basicCards.push(BasicCard(basicCardData[i].front, basicCardData[i].back));
+        }
+        console.log('Loading complete!');
+    }
+
+    initGame();
+
 
 } catch (e) {
     console.log(e);
